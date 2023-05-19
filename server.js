320891bf746387ac0a7cfbdc2b7440eb78198e17
Your Name <you@example.com>
2023-05-20 02:41:43 +0300

    Basma

---
+++ server.js
@@ -0,0 +1,72 @@
+import express from 'express';
+import { engine } from 'express-handlebars';
+import dotenv from 'dotenv'
+dotenv.config();
+
+import mongoose from 'mongoose';
+
+import department from './models/departmentdb.js'
+import doctorAccount from './models/doctorAccountdb.js'
+import studentAccount from './models/studentAccountdb.js'
+import subjects from './models/subjectdb.js'
+
+import studentsRouter from './routes/students.js';
+import doctorsRouter from './routes/doctors.js';
+import adminsRouter from './routes/admins.js';
+import homeRouter from './routes/home.js';
+import loginRouter from './routes/login.js';
+import subjectsRouter from './routes/subjects.js';
+import bodyParser from 'body-parser';
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.urlencoded({ extended: false }));
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './templates');
+
+app.use('/students', studentsRouter)
+app.use('/doctors', doctorsRouter)
+app.use('/admins', adminsRouter)
+app.use('/', homeRouter)
+app.use('/login', loginRouter)
+app.use('/subjects', subjectsRouter)
+
+app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect(process.env.mongoUrl).then((result) => {
+    app.listen(process.env.port, () => {
+        console.log(`The Application Started on http://localhost:${process.env.port}`);
+    });
+}).catch((err) => {
+    console.log(err);
+})
+
+app.post('/admins', (req, res) => {
+    const departmentt = new department(req.body);
+    departmentt.save()
+        .then((result) => { res.redirect('/admins') })
+        .catch((err) => { console.log(err) });
+});
+app.post('/doctors', (req, res) => {
+    const doctoraccount = new doctorAccount(req.body);
+    doctoraccount
+        .save()
+        .then((result) => { res.redirect('/doctors') })
+        .catch((err) => { console.log(err) });
+});
+app.post('/students', (req, res) => {
+    const studentaccount = new studentAccount(req.body);
+    studentaccount.save()
+        .then((result) => { res.redirect('/students') })
+        .catch((err) => { console.log(err) });
+});
+app.post('/subjects', (req, res) => {
+    const subjectt = new subjects(req.body);
+    subjectt.save()
+        .then((result) => { res.redirect('/subjects') })
+        .catch((err) => { console.log(err) });
+});
